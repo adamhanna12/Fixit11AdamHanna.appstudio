0ad5ee8959e0d77d816ed971a5eaedc07ddd5cc6
Adam <adamhanna@creighton.ed>
2019-11-03 21:51:09 -0600

    Final Commit

--- forms/describeYou/describeYou.js
+++ forms/describeYou/describeYou.js
@@ -1,5 +1,16 @@
 
+rdoPersonType.onclick=function(){
+    if (rdoPersonType.value == 0)
+      lblResponse1.value= "I would agree that you are a 'Optimistic' person too" 
+    else if (rdoPersonType.value == 1)
+      lblResponse1.value= "I would agree that you are a 'Pessimistic' person too"
+    else if (rdoPersonType.value == 2)
+      lblResponse1.value= "I would agree that you are a 'Trusting' person too"
+    else if (rdoPersonType.value == 3)
+      lblResponse1.value= "I would agree that you are a 'Envious' person too"
+}
 
-
-
+btnNextForm3.onclick=function(){
+  ChangeForm(favExercises)
+}
 
--- forms/favFoods/favFoods.js
+++ forms/favFoods/favFoods.js
@@ -1,1 +1,19 @@
 let goodFoods = ["pizza","apples","cookies","sushi","pasta","steak"]
+
+favFoods.onshow=function(){
+      for (i = 0; i <= goodFoods.length - 1; i++) 
+    lgFoods.addItem(goodFoods[i])
+}
+
+
+lgFoods.onclick=function(choice){
+    if (typeof(choice) == "object")   // user clicked the control
+    return
+   
+   lblResults.value="You picked " + goodFoods[choice]+ ", that is a great choice!"
+
+}
+
+btnNextForm1.onclick=function(){
+  ChangeForm(dessertVoting)
+}
